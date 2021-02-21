ba1a006b3eac3d89f28876f5a30a3fb30a4a1dc3
Elizabeth <39107604+thelizzerd@users.noreply.github.com>
2021-02-21 01:32:13 -0500

    add shared module and changed logo to center

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:  `
-    <nav class="navbar">
+    <nav class="center">
     <!== logo -->
     <div class = "navbar-logo">
         <h1><img src="../assets/img/logo.png"></h1>
